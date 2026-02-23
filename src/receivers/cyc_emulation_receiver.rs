0d07df30917a31ee8ac778666d9cd5ada0108ecc
Lux <iansseijelly@berkeley.edu>
2026-02-23 10:56:51 -0800

    feat: completed tc and cyc emulation receiver for BB level granularity

--- src/receivers/cyc_emulation_receiver.rs
+++ src/receivers/cyc_emulation_receiver.rs
@@ -4,19 +4,20 @@ use bus::BusReader;
 use std::fs::File;
 use std::io::{BufWriter, Write};
 
-pub struct TcEmulationReceiver {
+pub struct CycEmulationReceiver {
     writer: BufWriter<File>,
     receiver: BusReceiver,
-    curr_tc: u64,
-    interval: u64,
+    curr_cyc: u64,
+    lim_tnt: u64,
+    n_tnt: u64,
     event_staging: Vec<EventKind>,
 }
 
-/* Emulates the behavior of a TC-based encoder, used for accuracy analysis
-  A TC-based encoder encodes timestamp as separate packets emitted in fixed intervals.
+/* Emulates the behavior of a CYC-based encoder, used for accuracy analysis
+  A CYC-based encoder encodes cycles as a property of each trace event.
 */
-impl TcEmulationReceiver {
-    pub fn new(bus_rx: BusReader<Entry>, path: String, interval: u64) -> Self {
+impl CycEmulationReceiver {
+    pub fn new(bus_rx: BusReader<Entry>, path: String, lim_tnt: u64) -> Self {
         Self {
             writer: BufWriter::new(File::create(path).unwrap()),
             receiver: BusReceiver {
@@ -24,8 +25,9 @@ impl TcEmulationReceiver {
                 bus_rx: bus_rx,
                 checksum: 0,
             },
-            curr_tc: 0,
-            interval: interval,
+            curr_cyc: 0,
+            lim_tnt: lim_tnt,
+            n_tnt: 0,
             event_staging: Vec::new(),
         }
     }
@@ -41,16 +43,16 @@ pub fn factory(
         .and_then(|value| value.as_str())
         .unwrap_or("trace.tc_emulation.txt")
         .to_string();
-    let interval = _config
-        .get("interval")
+    let lim_tnt = _config
+        .get("lim_tnt")
         .and_then(|value| value.as_u64())
         .unwrap_or(1000000);
-    Box::new(TcEmulationReceiver::new(bus_rx, path, interval))
+    Box::new(CycEmulationReceiver::new(bus_rx, path, lim_tnt))
 }
 
-crate::register_receiver!("tc_emulation", factory);
+crate::register_receiver!("cyc_emulation", factory);
 
-impl AbstractReceiver for TcEmulationReceiver {
+impl AbstractReceiver for CycEmulationReceiver {
     fn bus_rx(&mut self) -> &mut BusReader<Entry> {
         &mut self.receiver.bus_rx
     }
@@ -61,22 +63,34 @@ impl AbstractReceiver for TcEmulationReceiver {
 
     fn _receive_entry(&mut self, entry: Entry) {
         match entry {
-            Entry::Event {
-                timestamp,
-                kind: EventKind::SyncStart { .. },
-            } => {
-                self.curr_tc = timestamp / self.interval;
-            }
             Entry::Event { timestamp, kind } => {
-                if timestamp / self.interval != self.curr_tc {
+                // first, always stage the event
+                // this is safe because all events BEFORE a CYC packet is guaranteed to happen beforehand
+                match kind {
+                    EventKind::SyncStart { .. } => {
+                        self.curr_cyc = timestamp;
+                    }
+                    EventKind::TakenBranch { .. } => {
+                        self.n_tnt += 1;
+                        self.event_staging.push(kind.clone());
+                    }
+                    EventKind::NonTakenBranch { .. } => {
+                        self.n_tnt += 1;
+                        self.event_staging.push(kind.clone());
+                    }
+                    _ => {
+                        self.event_staging.push(kind.clone());
+                    }
+                }
+                if self.n_tnt >= self.lim_tnt || matches!(&kind, EventKind::UninferableJump { .. })
+                {
                     // smear the timestamps distributing across all events staged, excluding the current event
-                    let next_tc = timestamp / self.interval;
-                    let slack = (next_tc - self.curr_tc) * self.interval;
+                    let slack = timestamp - self.curr_cyc;
                     let num_events = self.event_staging.len() as u64;
-                    let delta_tc = slack / num_events;
+                    let delta_cyc = slack / num_events;
                     for event in self.event_staging.iter() {
                         self.writer
-                            .write_all(format!("[timestamp delta: {}]", delta_tc).as_bytes())
+                            .write_all(format!("[timestamp delta: {}]", delta_cyc).as_bytes())
                             .unwrap();
                         self.writer
                             .write_all(format!(" {}", event).as_bytes())
@@ -85,10 +99,10 @@ impl AbstractReceiver for TcEmulationReceiver {
                     }
                     // clear the staged events
                     self.event_staging.clear();
+                    self.n_tnt = 0;
                     // update the current TC
-                    self.curr_tc = next_tc;
+                    self.curr_cyc = timestamp;
                 }
-                self.event_staging.push(kind);
             }
             _ => {}
         }
